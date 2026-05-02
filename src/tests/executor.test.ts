e19d0caf72e53302bcd43289fb7f87ab10a6392c
R-Abinav <r.abinav1712@gmail.com>
2026-05-02 19:16:26 +0530

    chore: ollama inference

---
+++ src/tests/executor.test.ts
@@ -0,0 +1,8 @@
+import { runInference } from "../executor";
+
+async function testInference(){
+    const result = await runInference("tinyllama", "whats 68 + 100 - 70 in one sentence?");
+    console.log(result);
+}
+
+testInference();
